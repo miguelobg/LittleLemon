17496d4dfdd071a4b5fa7258f1baa546d0596155
miguelobg <miguel_obg@hotmail.com>
2025-03-08 18:57:48 +0000

    curso Jetpack Compose coursera

--- app/src/main/java/com/example/littlelemon/ui/components/ItemOrder.kt
+++ app/src/main/java/com/example/littlelemon/ui/components/ItemOrder.kt
@@ -1,5 +1,6 @@
 package com.example.littlelemon.components
 
+import android.provider.CalendarContract.Colors
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,6 +11,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -17,17 +22,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun ItemOrder() {
-    var count by remember {
+    var count by rememberSaveable() {
         mutableStateOf(0)
     }
     Column(
@@ -35,45 +43,74 @@ fun ItemOrder() {
             .fillMaxSize()
             .background(Color.White)
             .padding(8.dp),
-        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
-
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Greek Salad",
-            fontSize = 26.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .padding(8.dp)
-        )
-
-        Row (
+        Card(
+            colors = CardDefaults.cardColors(containerColor = Color(0xFF495E57)),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp), horizontalArrangement = Arrangement.Center
-        ){
-            IconButton(
-                onClick = {if (count > 0) count--}
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Remove,
-                    contentDescription = "Remove"
-                )
-            }
+        ) {
             Text(
-                text = "$count",
-                fontSize = 28.sp,
+                text = "Greek Salad",
+                color = Color(0xFFF4CE14),
+                fontSize = 26.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(8.dp)
+                modifier = Modifier
+                    .padding(8.dp)
             )
 
-            IconButton(
-                onClick = {count++}
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+            ){
+                IconButton(
+                    onClick = {if (count > 0) count--},
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Remove,
+                        contentDescription = "Remove",
+                        tint = Color.White
+                    )
+                }
+                Text(
+                    text = "$count",
+                    fontSize = 28.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(8.dp)
+                )
+
+                IconButton(
+                    onClick = {count++},
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add",
+                        tint = Color.White
+                    )
+                }
+            }
+            Button(
+                onClick = {/* todo */},
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF4CE14)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+
             ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "Add"
+                Text(
+                    text = "Add to Order",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF333333)
                 )
             }
+
+
         }
     }
 }
